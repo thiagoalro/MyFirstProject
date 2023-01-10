cfcd1003290299fffc03e26726a5ef5063a1d1fa
thiago <thiago.alves@ifce.edu.br>
2023-01-10 14:17:11 -0300

    adiciona uma entrada de inteiro e apresenta o inteiro

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
 		System.out.printf("%s%n", "Hello World!");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Digite um numero inteiro: ");
+		int numero = input.nextInt();
+		System.out.printf("Você digitou: %d%n", numero);
 
 	}
 }
