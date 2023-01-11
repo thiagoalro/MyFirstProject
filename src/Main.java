93d1fbf39ec9d3b26f08b4f9df99ce9a87b0bd6b
thiago <thiago.alves@ifce.edu.br>
2023-01-11 01:40:33 -0300

    adiciona outro hello

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
+		System.out.printf("%s%n", "Hello World2!");
+
 		System.out.printf("%s%n", "Hello World!");
 		Scanner input = new Scanner(System.in);
 
