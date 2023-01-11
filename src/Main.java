026a805f3b73434a12241b15ce951ef4d7e06c2c
thiago <thiago.alves@ifce.edu.br>
2023-01-11 02:24:23 -0300

    cria classe Nova, remove comments e tira print redundante

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.printf("%s%n", "Hello World2!");
-
 		System.out.printf("%s%n", "Hello World!");
 		Scanner input = new Scanner(System.in);
-
-//		System.out.print("Digite um numero double: ");
-//		double numeroDouble = input.nextDouble();
-//		System.out.printf("Você digitou: %.2f%n", numeroDouble);
 
 		System.out.print("Digite uma string: ");
 		String word = input.nextLine();
---
+++ src/Nova.java
@@ -0,0 +1,9 @@
+public class Nova {
+
+	public static void main(String[] args){
+		System.out.print("Programa encerrado!");
+	}
+
+
+
+}
